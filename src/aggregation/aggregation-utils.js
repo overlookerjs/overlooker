287c6ca9b7d4daf7da8327498fd98388b5d990dc
Nikolay Ryabov <pyatyispyatil@gmail.com>
2019-07-23 23:30:32 +0300

    optimize aggregation

--- src/aggregation/aggregation-utils.js
+++ src/aggregation/aggregation-utils.js
@@ -1,32 +1,28 @@
 const { objMap } = require('./../utils.js');
 
 const percentile = (arr, percentile) => (
-  arr
-    .slice()
-    .sort((a, b) => a - b)
-    [Math.floor(arr.length * percentile)]
+  arr[Math.floor(arr.length * percentile)]
 );
 
 const avg = (arr) => arr.reduce((acc, item) => acc + item) / arr.length;
-
-const max = (arr) => arr.reduce((res, item) => res > item ? res : item);
-
-const min = (arr) => arr.reduce((res, item) => res < item ? res : item);
 
 const standardDeviation = (array, mean) => Math.sqrt(array.reduce((acc, item) => acc + Math.pow(item - mean, 2), 0) / array.length);
 
 const objAggregation = (obj) => objMap(obj, (array) => {
   const mean = avg(array);
+  const sortedArray = array
+    .slice()
+    .sort((a, b) => a - b);
 
   return {
     raw: array,
-    q1: percentile(array, 0.25),
-    q3: percentile(array, 0.75),
-    percentile98: percentile(array, 0.98),
-    percentile02: percentile(array, 0.02),
-    max: max(array),
-    min: min(array),
-    median: percentile(array, 0.5),
+    q1: percentile(sortedArray, 0.25),
+    q3: percentile(sortedArray, 0.75),
+    percentile98: percentile(sortedArray, 0.98),
+    percentile02: percentile(sortedArray, 0.02),
+    max: sortedArray[sortedArray.length - 1],
+    min: sortedArray[0],
+    median: percentile(sortedArray, 0.5),
     count: array.length,
     standardDeviation: standardDeviation(array, mean),
     mean
@@ -34,10 +30,5 @@ const objAggregation = (obj) => objMap(obj, (array) => {
 });
 
 module.exports = {
-  standardDeviation,
-  percentile,
-  avg,
-  max,
-  min,
   objAggregation
 };
