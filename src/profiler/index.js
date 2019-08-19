179f73aa88b740c9f87d70fd7a5a5830321083df
Nikolay Ryabov <pyatyispyatil@gmail.com>
2019-08-19 22:38:56 +0300

    Update README.md

--- src/profiler/index.js
+++ src/profiler/index.js
@@ -6,14 +6,14 @@ const { prepareResult } = require('./preparing.js');
 
 /**
  * @param {Object} config
- * @param {Object[]} config.pages - array of pages objects to profile
- * @param {string} config.pages.$.name - name of page
- * @param {string} config.pages.$.url - url of page
- * @param {Object} [config.pages.$.actions] - actions array, which executed after onLoad event
- * @param {Function} [config.pages.$.actions.actionName] - function receive instance of page in arguments and must return promise
+ * @param {Object[]} config.pages - an array of page objects for the profile
+ * @param {string} config.pages.$.name - page name
+ * @param {string} config.pages.$.url - page url
+ * @param {Object} [config.pages.$.actions] - array of actions that are executed after the onLoad event
+ * @param {Function} [config.pages.$.actions.actionName] - the function receives an instance of the page in arguments and should return a promise
  * @param {Object} [config.throttling] - throttling object
- * @param {number} [config.throttling.cpu] - throttling of the cpu (higher - worst)
- * @param {string} [config.throttling.network] - throttling of the network (GPRS|Regular2G|Good2G|Regular3G|Good3G|Regular4G|DSL|WiFi)
+ * @param {number} [config.throttling.cpu] - cpu throttling (higher - worst)
+ * @param {string} [config.throttling.network] - network throttling (GPRS|Regular2G|Good2G|Regular3G|Good3G|Regular4G|DSL|WiFi)
  * @param {number} [config.count] - the number of measurements for each page
  * @param {number} [config.threads] - the number of browser instances for profiling (higher - measurement will be faster, but less accurate)
  * @param {string} [config.platform] - platform for profile (desktop|mobile)
@@ -22,7 +22,7 @@ const { prepareResult } = require('./preparing.js');
  * @param {Object} [config.requests] - object for manipulate network requests
  * @param {string|RegExp|Function|Array} [config.requests.ignore] - for ignore requests
  * @param {string|RegExp|Function|Array} [config.requests.merge] - for merge requests while aggregation
- * @param {string|RegExp|Function|Array} [config.requests.internalTest] - to mark every request as internal/external
+ * @param {string|RegExp|Function|Array} [config.requests.internalTest] - mark each request as internal / external
  * */
 
 const profile = async (config) => {
